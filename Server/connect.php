9c74378b51b605b897fa07a4073805f404fe9a8d
Mahdi Bagheri <bagheri.almas@gmail.com>
2022-07-06 17:32:18 +0430

    Update connect.php

--- Server/connect.php
+++ Server/connect.php
@@ -1,1 +1,16 @@
+<?php
 
+define("DB_HOST", "localhost");
+define("DB_USER", "<USERNAME>");
+define("DB_PASSWORD", "<PASSWORD>");
+define("DB_DATABASE", "<DATABASE NAME>");
+
+$conn = mysqli_connect(DB_HOST,DB_USER,DB_PASSWORD,DB_DATABASE);
+	
+// 	if($conn){
+// 		echo "success";
+// 	}else
+// 	{
+// 	 echo "No connection";   
+// 	}
+?>
